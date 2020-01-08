dcaf1809aff40c1b298eaaa54a2e9619bf176f6a
Michael Tonini <michael.jack.tonini@gmail.com>
2020-01-08 11:10:28 +1100

    dev test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Scriibi
+                    Scriibi - Testing Dev Branch Pipeline
                 </div>
 
                 <div class="links">
