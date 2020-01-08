165c2c1319b313e858929b73993c1b9888a3895a
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-08 17:11:31 +1100

    Create PostsController.php

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    // public function show($slug){
+    //   $post= \DB::table('post')->where('slug',$slug)->first();
+    // }
+    // dd($post);
+
+    public function show(){
+      return "hello";
+    }
+}
