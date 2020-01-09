8ea7c4a57ff261c556acfa6666056b3660b56fdf
Chamath Vindula <chamathvin@gmail.com>
2020-01-09 17:42:30 +1100

    second database modelling commit by chamath

---
+++ app/Http/Controllers/SkillsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\skills;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SkillsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\skills  $skills
+     * @return \Illuminate\Http\Response
+     */
+    public function show(skills $skills)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\skills  $skills
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(skills $skills)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\skills  $skills
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, skills $skills)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\skills  $skills
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(skills $skills)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_01_09_060551_create_positions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->bigIncrements('position_Id');
+            $table->string('position_Name', 45);
+            $table->unique('position_Id');
+            $table->unique('position_Name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
