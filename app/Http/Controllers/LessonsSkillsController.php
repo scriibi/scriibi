5bb9eeedfcade53a33038b71181de179c2048bbe
Chamath Vindula <chamathvin@gmail.com>
2020-01-10 11:44:47 +1100

    fourth database modelling commit by chamath

---
+++ app/Http/Controllers/LessonsSkillsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\lessons_skills;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LessonsSkillsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\lessons_skills  $lessons_skills
+     * @return \Illuminate\Http\Response
+     */
+    public function show(lessons_skills $lessons_skills)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\lessons_skills  $lessons_skills
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(lessons_skills $lessons_skills)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\lessons_skills  $lessons_skills
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, lessons_skills $lessons_skills)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\lessons_skills  $lessons_skills
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(lessons_skills $lessons_skills)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TextTypesController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\text_types;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TextTypesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\text_types  $text_types
+     * @return \Illuminate\Http\Response
+     */
+    public function show(text_types $text_types)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\text_types  $text_types
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(text_types $text_types)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\text_types  $text_types
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, text_types $text_types)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\text_types  $text_types
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(text_types $text_types)
+    {
+        //
+    }
+}
