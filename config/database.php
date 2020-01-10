a441c6e35ca16d93d5b9a08d07c0c360ad1c92ac
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-11 00:06:22 +1100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,13 @@
 <?php
 use Illuminate\Support\Str;
 
+/*
 define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
 define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
 define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
 define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
 define('RDS_PORT', $_SERVER['RDS_PORT']);
+*/
 
 return [
     /*
@@ -42,15 +44,14 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-        'mysql' => [
+	'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'driver' => 'mysql',
-            'host' => RDS_HOSTNAME,
-            'port' => RDS_PORT,
-            'database' => RDS_DB_NAME,
-            'username' => RDS_USERNAME,
-            'password' => RDS_PASSWORD,
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
