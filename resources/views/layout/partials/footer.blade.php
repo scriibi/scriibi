dcbf14e2aee6977dc54f803efcb0fccade32b3e1
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-10 12:57:36 +1100

    Create footer.blade.php

---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="text-muted" class="footer">
+     <div class="container">
+       <p>&copy; 2019 scriibi.All rights reserved<p>
+       <p class="float-right">
+         Scriibi takes privacy seriously. Visit Our privacy policy to learn more
+       </p>
+
+     </div>
+</footer>
