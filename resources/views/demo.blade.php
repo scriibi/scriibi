e457fd9bc5ec81c0000322a4d0c7cc81abe69acd
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-10 12:57:20 +1100

    Create demo.blade.php

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.mainlayout')
+@section('content')
+   <div class="album text-muted">
+     <div class="container">
+       <div class="row">
+         <h1>Student list</h1>
+          <ul>
+            <li>Student 1</li>
+            <li>Student 2</li>
+            <li>Student 3</li>
+          </ul>
+       </div>
+    </div>
+  </div>
+@endsection
