0b7b7049b6cb3f8c319277a858fff3820b02f4e0
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-10 12:57:47 +1100

    Create header.blade.php

---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,21 @@
+
+<div class="row">
+  <div class="col-sm-4">
+    <img src="https://www.scriibi.com/wp-content/uploads/2018/04/scriibi-logo-tight-e1525256307991.png
+  "alt="scriibi-logo">
+  </div>
+  <div class="col-sm-4">
+    <ul class="nav">
+      <li class="nav-item" href="#">Assessment</li>
+      <li class="nav-item" href="#">Rubrics</li>
+      <li class="nav-item" href="#">Support Material</li>
+      <li class="nav-item" href="#">Logout</li>
+    </ul>
+
+  </div>
+
+  <div class="col-sm-4">
+    <a class= "" type="button" role="button" href="#">Logout</a>
+
+  </div>
+</div>
