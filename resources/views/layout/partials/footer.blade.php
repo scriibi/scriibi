4496202711ac1b88237b4d666991a1d050cbe169
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-12 12:00:38 +1100

    Update footer.blade.php

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="page-footer font-small white pt-3 bg-dark">
      <div class="footer-copyright text-center py-3 bg-secondary text-white">
        <p class="text-light">
-         Scriibi takes privacy seriously. Visit Our <a href="#" class="text-muted">privacy policy</a> to learn more
+         Scriibi takes privacy seriously. Visit Our <a href="#" class="text-white-50">privacy policy</a> to learn more
        </p>
        <p class="text-light">&copy; 2019 scriibi.All rights reserved<p>
      </div>
