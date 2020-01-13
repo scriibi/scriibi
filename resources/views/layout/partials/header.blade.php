a6ed1b371b01124cf59ab0f14affdf7df28a67c8
Angelo Innocente <angelot.innocente@gmail.com>
2020-01-14 10:43:18 +1100

    rework footer and header
    minor tweaks to footer to stay at the bottom and header to be capitalised and bigger

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,13 +1,13 @@
-<nav class="row nav-header">
+<nav class="row nav-header pt-2 pb-2">
     <div class="d-none d-sm-block col-sm-1 col-md-2"></div>
     <!-- Navbar content -->
     <div class="d-inline-block col-12 col-sm-10 col-md-8 pt-2 pb-2"> 
         <ul class="nav-header-menu">
             <li><a href="#"><img class="logo" src="https://www.scriibi.com/wp-content/uploads/2018/04/scriibi-logo-tight-e1525256307991.png" /></a></li>
-            <li><a href="#">Assessment</a></li>
-            <li><a href="#">Rubircs</a></li>
-            <li><a href="#">Support Material</a></li>
-            <li class="float-right"><a href="#">Logout</a></li>
+            <li><a href="#">ASSESSMENT</a></li>
+            <li><a href="#">RUBRIC</a></li>
+            <li><a href="#">SUPPORT MATERIAL</a></li>
+            <li class="float-right"><a href="#">LOG OUT</a></li>
         </ul>
     </div>
     <!-- /navbar content -->
