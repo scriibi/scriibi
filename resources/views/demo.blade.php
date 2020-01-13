928598a508cbe052ea28a01eb822ae7a44cc70b5
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-13 20:50:08 +1100

    css

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -8,7 +8,6 @@
        <div class="content-container_grid_3">
         Leftmost
        </div>
-
        <div class="grid_middle">
          <p id="list_title">This is a master list of all your students</p>
          <p>The information below will be used to populate assessments</p>
@@ -16,58 +15,46 @@
          <div class="card">
            <div class="card-body">
              <h5 class="card-title">Add students to your list</h5>
-             <form>
-                <div class="form-row">
-                  <div class="form-group col-md-2">
-                    <label for="inputEmail">First name</label>
-                    <input type="text" class="form-control" id="inputEmail">
-                  </div>
-                  <div class="form-group col-md-2">
-                    <label for="inputPassword4">Last name</label>
-                    <input type="text" class="form-control" id="inputPassword4">
-                  </div>
-                  <div class="form-group col-md-2">
-                    <label for="inputPassword4">ID</label>
-                    <input type="text" class="form-control" id="inputPassword4">
-                  </div>
-                  <div class="form-group col-md-2">
-                    <label for="inputPassword4">Grade</label>
-                    <input type="text" class="form-control" id="inputPassword4">
-                  </div>
-                  <div class="form-group col-md-2">
-                    <label for="inputPassword4">Assessemnt Level<span>&#8505;</span></label>
-                    <input type="text" class="form-control" id="inputPassword4">
-                  </div>
-                  <div class="form-group col-md-2">
-                    <label for="inputPassword4"></label>
-                    <button type="submit" class="btn btn-primary">Add</button>
-                  </div>
-                </div>
-              </form>
-              <table class="table">
-                <thead>
+             <form class="form-row" action="#" method="post">
+               <table class="table card-body">
+                 <thead>
+                   <tr>
+                     <th>First name</th>
+                     <th>Last name</th>
+                     <th>ID</th>
+                     <th>Grade</th>
+                     <th >Assessemnt Level</th>
+                     <th></th>
+                 </thead>
+                 <tbody class="form-group">
+                   <tr>
+                     <td><input type="text" class="form-control" id="inputEmail"></td>
+                     <td><input type="text" class="form-control" id="inputPassword4"></td>
+                     <td><input type="text" class="form-control" id="inputPassword4"></td>
+                     <td>
+                       <select class="form-control" id="selectGrade">
+                         <option>Grade 1</option>
+                         <option>Grade 2</option>
+                         <option>Grade 3</option>
+                         <option>Grade 4</option>
+                         <option>Grade 5</option>
+                       </select>
+                     </td>
+                     <td>
+                       <select class="form-control" id="selectAssessmentLevel">
+                         <option>Grade 1</option>
+                         <option>Grade 2</option>
+                         <option>Grade 3</option>
+                         <option>Grade 4</option>
+                         <option>Grade 5</option>
+                       </select>
+                     </td>
+                     <td><button type="submit" class="btn btn-primary">Add</button></td>
+                   </tr>
 
-                  <tr>
-                    <th>First name</th>
-                    <th>Last name</th>
-                    <th>ID</th>
-                    <th>Grade</th>
-                    <th>Assessemnt Level</th>
-                    <th></th>
-
-                </thead>
-                <tbody>
-                  <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                  </tr>
-
-                </tbody>
-              </table>
+                 </tbody>
+               </table>
+             </form>
            </div>
          </div>
        </div>
