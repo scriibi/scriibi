6598ba0241eb02fc47fd862a544a7f157acc5c40
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-14 10:19:14 +1100

    Update pagetemplate.php

--- resources/views/layout/partials/pagetemplate.php
+++ resources/views/layout/partials/pagetemplate.php
@@ -2,8 +2,9 @@
    <div class="d-none d-sm-block col-sm-1 col-md-2">
    </div>
     <div class="col-12 col-sm-10 col-md-8">
-        
-        
+
+
    </div>
     <div class="d-none d-sm-block col-sm-1 col-md-2">
    </div>
+</div>
