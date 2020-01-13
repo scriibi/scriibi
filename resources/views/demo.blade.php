331850c4ecad347e33719052190899f13fe1a2d2
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-13 16:23:20 +1100

    package update

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -1,16 +1,32 @@
 @extends('layout.mainlayout')
 @section('content')
-   <div class="album text-muted">
-     <div class="container">
-       <div class="row">
-         <h1>Student list</h1>
-          <ul>
-            <li>Student 1</li>
-            <li>Student 2</li>
-            <li>Student 3</li>
-          </ul>
+
+
+   <div class="content">
+     <div class="content-container">
+       <div class="content-container_grid_3">
+        Leftmost
        </div>
 
+       <div class="grid_middle">
+         <h1>This is a master list of all your students</h1>
+         <p>The information below will be used to populate assessments and will appear when the data is viewed</p>
+
+
+
+       </div>
+
+       <div class="content-container_grid_3">
+        Rightmost
+       </div>
     </div>
+
+
+  
+
+
+
+
+
   </div>
 @endsection
