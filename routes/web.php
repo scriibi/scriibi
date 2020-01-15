bce7bd8b01661d4c3547757bc1aca34254d2d377
Michael Tonini <michael.jack.tonini@gmail.com>
2020-01-15 11:33:46 +1100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/welcome', function () {
+Route::get('/', function () {
    return view('welcome');
 });
-Route::get('/', function () {
+
+Route::get('/demo', function () {
     return view('demo');
- });
+});
 
 
 Route::get('/rubrics', function(){
