9c3700352312fec0b63f2b54a7447a5ae758d771
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-15 17:06:47 +1100

    afternoon Wed

--- resources/views/rubrics.blade.php
+++ resources/views/rubrics.blade.php
@@ -10,28 +10,86 @@
         <!-- rubric Builder -->
         <h4 class="top-divider mb-3 header-text"><strong>Rubric Builder</strong></h4>
         <div class="universal-card p-2">
-            <form class="" action="index.html" method="post">
-                <div class="form-group d-inline-flex p-2 bd-highlight"  id="step2-form">
-                    <div class="col d-flex justify-content-between">
-                        <input type="text" class="form-control" name="" value="Term 1 Rubric">
-                    </div>
-                    <div class="col input-group">
-                        <div class="input-group-prepend">
-                            <select class="" name="">
-                                <option value="Code value">Curriculum Codes</option>
-                                <option value="">Level 1</option>
-                                <option value="">Level 2</option>
-                                <option value="">Level 3</option>
-                                <option value="">Level 4</option>
-                            <option value="">Level 5</option>
-                            </select>
+            <form action="index.html" method="post">
+                <!-- term-title+Curriculum -->
+                <div class="form-group">
+                    <div class="d-flex justify-content-around">
+                        <div class="col-sm-8 input-group-lg term-title-box">
+                            <input type="text" class="form-control " name="" value="Term 1 Rubric">
                         </div>
+                        <select class="col-sm-3 input-group-lg" name="">
+                            <option value="Code value">Curriculum Codes</option>
+                            <option value="">Level 1</option>
+                            <option value="">Level 2</option>
+                            <option value="">Level 3</option>
+                            <option value="">Level 4</option>
+                            <option value="">Level 5</option>
+                        </select>
                     </div>
+                </div>
+
+                <!-- txt-types checkbox -->
+                <div class="d-flex btn-group-toggle justify-content-start" datat-toggle="buttons">
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button-" name="" value="description">Description
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="discussion">Discussion
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="explanation">Explanation
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="historical-narrative">Historical Narrative
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="information-report">Information Report
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="narrative">Narrative
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="persuasive">Persuasive
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="poetry">Poetry
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="procedure">Procedure
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="recount">Recount
+                        </label>
+                    </div>
+                    <div class="btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-outline-success text-nowrap">
+                            <input type="checkbox" role="button" name="" value="response">Response
+                        </label>
+                    </div>
+
                 </div>
             </form>
         </div>
 
-   </div>
    <div class="d-none d-sm-block col-sm-1 col-md-2">
    </div>
 </div>
