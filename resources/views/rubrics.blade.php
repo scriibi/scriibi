6bc35ee85d7654fc14a0c0dde57914c8b8222fdc
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-16 15:02:15 +1100

    afternoon1

--- resources/views/rubrics.blade.php
+++ resources/views/rubrics.blade.php
@@ -19,7 +19,7 @@
                     <!-- term-title+Curriculum -->
                     <div class="card-text">
                         <div class=" form-group d-flex justify-content-around ">
-                            <input type="text" class="col-9 input-group-sm rubric-border-box " name="" value="Term 1 Rubric">
+                            <input type="text" class="col-9 input-group-sm rubric-border-box pr-1" name="" value="Term 1 Rubric">
                             <select class="col-2.5 input-group-sm rubric-border-box" name="">
                                 <option value="Code value">Curriculum Codes</option>
                                 <option value="">Level 1</option>
@@ -30,132 +30,95 @@
                             </select>
                         </div>
 
-                        <!-- txt-types checkboxs -->
-                        <div class="d-flex flex-wrap btn-group-toggle col-11" datat-toggle="buttons">
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                        <!-- pop txt-types checkboxs from DB-->
+                        <div class="d-flex flex-wrap btn-group-toggle px-2" datat-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button-" name="" value="description">Description
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button" name="" value="discussion">Discussion
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button" name="" value="explanation">Explanation
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button" name="" value="historical-narrative">Historical Narrative
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button" name="" value="information-report">Information Report
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button" name="" value="narrative">Narrative
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button" name="" value="persuasive">Persuasive
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button" name="" value="poetry">Poetry
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button" name="" value="procedure">Procedure
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type ">
                                     <input type="checkbox" role="button" name="" value="recount">Recount
                                 </label>
                             </div>
-                            <div class="btn-group-toggle p-1" data-toggle="buttons">
+                            <div class="btn-group-toggle px-1 pb-1" data-toggle="buttons">
                                 <label class="btn btn-sm btn-outline-success text-nowrap btn-text-type">
                                     <input type="checkbox" role="button" name="" value="response">Response
                                 </label>
                             </div>
                         </div>
-                        <!-- skills cards      -->
-                        <div class="card-columns">
-                            <!-- card for ideas -->
-                            <div class="card">
-                                <div class="card-body">
+                        <!-- skills cards-->
+                        <div class="card-columns pt-5 col-11">
+                            <!-- load cards from skill-categories DB; each card has icon address, skill-title, skillset-items, color code, ex: #FFD12D -->
+                            <div class="card h-auto d-inline-block">
+                                <!-- content inside each skillcategory -->
+                                <div class="card-body skill-card-style">
+                                    <div class="card-text">
+                                        <!-- load icon address -->
+                                        <span class="align-middle">icon</span>
+                                        <!-- load skill title -->
+                                        <span class="skill-title align-middle w-100 pl-0">ideas</span>
+                                    </div>
 
+                                    <div class="card-text form-check body-list bg-white">
+                                      <div class="d-block border-for-checkbox">
+                                        <input class="form-check-input checkbox-circle" type="checkbox" name="" value="">
+                                        <label class="form-check-label" for="">12</label>
+                                      </div>
+                                      <div class="">
+                                        <input class="form-check-input"type="checkbox" name="" value="">
+                                        <label class="form-check-label" for="">56</label>
+                                      </div>
+                                    </div>
                                 </div>
-
-                            </div>
-
-                            <!-- card forrganisation -->
-                            <div class="card">
-
-                            </div>
-
-                            <!-- card for word choice -->
-                            <div class="card">
-
-                            </div>
-
-                            <!-- card for Conventions -->
-                            <div class="card">
-
-                            </div>
-
-                            <!-- card for sentence fluency -->
-                            <div class="card">
-
-                            </div>
-
-                            <!-- card for Voice -->
-                            <div class="card">
-
-                            </div>
-
-                            <!-- card for Other skills -->
-                            <div class="card">
-
                             </div>
                         </div>
+                            <!-- end of skill cards columns -->
                     </div>
                 </form>
-
             </div>
-            <div class="card-body">
-
-            </div>
-            <div class="card-body">
-
-            </div>
-            <div class="card-body">
-
-            </div>
-            <div class="card-body">
-
-            </div>
-            <div class="card-body">
-
-            </div>
-            <div class="card-body">
-
-            </div>
-            <div class="card-body">
-
-            </div>
-
         </div>
-
    <div class="d-none d-sm-block col-sm-1 col-md-2">
    </div>
 </div>
