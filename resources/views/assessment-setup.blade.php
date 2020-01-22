e730892370b87fbe04e3d25187ad5695add1dfd6
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-22 13:06:16 +1100

    wed

--- resources/views/assessment-setup.blade.php
+++ resources/views/assessment-setup.blade.php
@@ -12,10 +12,10 @@
         <!-- accordion for assessment setup -->
         <div class="accordion card-assessment-style" id="assessmentAccordion">
             <!-- step 1: assessment detail -->
-            <div class="card">
+            <div class="card collapse show" id="assessment-template">
                 <div class="card-body">
                     <div class="card-title mb-0">
-                        <h5>Step 1: assessment details</h5>
+                        <h5>Assessment Details</h5>
                     </div>
                     <div class="card-text mb-0 mt-4 row" id=>
                         <div class="col-sm-8">
@@ -35,19 +35,28 @@
                         <label for="" class="col-sm-12 m-0 p-0">Description</label>
                         <input type="textarea" name="" value="" class="col-sm-12 mt-1" id="description-for-assessment">
                     </div>
+                    <h5 class="assessment-settings-title mt-5">Assessment Settings</h5>
+                    <div class="d-flex justify-content-start mt-3">
+                        <button type="button" name="button" class="btn assessment-settings-btn">Assess <strong>my</strong> students </button>
+                        <button type="button" name="button" class="btn assessment-settings-btn ml-4">Assess <strong>all</strong> students </button>
+                    </div>
+
                     <div class="d-flex justify-content-end mt-4 mb-2">
-                        <button type="button" name="button" class="btn btn-link assessment-btn border-0" data-toggle="collapse"
-                        data-target="#rubric-template" aria-expanded="true" aria-controls="rubric-template">Next</button>
+                        <span data-toggle="collapse show" data-target="#assessment-template" aria-expanded="false" aria-controls="create-assessment-title">
+                            <button type="button" name="button" class="btn btn-link assessment-btn border-0" data-toggle="collapse"
+                            data-target="#rubric-template" aria-expanded="true" aria-controls="rubric-template">Rubric Selection
+                            </button>
+                        </span>
                     </div>
                 </div>
             </div>
             <!-- step 2:Rubric Template -->
-            <div class="card" >
-                <div class="card-body pt-1 pb-0">
+            <div class="card collapse" id="rubric-template">
+                <div class="card-body pt-1 pb-0 " >
                     <div class="card-title" >
-                        <h5>Step 2: Rubric Template</h5>
+                        <h5>Rubric Selection</h5>
                     </div>
-                    <div class="collapse"  id="rubric-template">
+                    <div class="collapse"  >
                         <div class="card-text">
                             <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                             </p>
@@ -59,9 +68,9 @@
 
                         </div>
                     </div>
-
                 </div>
             </div>
+
             <!-- step 3:Student Option -->
             <div class="card">
                 <div class="card-body pt-1 pb-0">
