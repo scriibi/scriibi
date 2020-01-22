dcf08210d172294cf2d0b006e51c479e6abd1a11
Angelo Innocente <angelot.innocente@gmail.com>
2020-01-22 15:41:43 +1100

    added home

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -4,6 +4,7 @@
     <div class="d-inline-block col-12 col-sm-10 col-md-8 pt-2 pb-2">
         <ul class="nav-header-menu">
             <li><a class="nav-logo" href="/home"><img class="logo" src="https://www.scriibi.com/wp-content/uploads/2018/04/scriibi-logo-tight-e1525256307991.png" /></a></li>
+            <li><a href="/">HOME</a></li>
             <li><a href="/assessment-list">ASSESSMENT</a></li>
             <li><a href="/rubric-list">RUBRIC</a></li>
             <li><a href="#">SUPPORT MATERIAL</a></li>
