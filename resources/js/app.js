46065118521ec8c7c85d809663d0d7e7535769b0
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-24 12:30:33 +1100

    Lunch

--- resources/js/app.js
+++ resources/js/app.js
@@ -151,7 +151,6 @@ $(document).ready(function () {
     });
 
     // arrow rotate function
-
     $(".arrow-up-btn").click(function(){
         $(this).find(".collapsable-arrow").toggleClass("image-rotate");
     });
