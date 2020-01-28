a736339c9898333a71d3822be5388cfcee0058e8
Dara-Chi <37431105+DaraHarris@users.noreply.github.com>
2020-01-28 20:57:47 +1100

    rubric input validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -142,10 +142,15 @@ function toggleRadioBorder(event) {
         sibling.parentNode.classList.remove("checked");
     }
 }
+if (assessClass !== null) {
+    assessClass.addEventListener("click", toggleRadioBorder, true);
+}
+if (assessGrade !== null) {
+    assessGrade.addEventListener("click", toggleRadioBorder, true);
+}//end of radio button script
 
-assessClass.addEventListener("click", toggleRadioBorder, true);
-assessGrade.addEventListener("click", toggleRadioBorder, true);
-//end of radio button script
+
+
 
 // the rubric selection button
 var rubricSelectionBTN = document.getElementById("rubricSelectionBTN");
@@ -163,42 +168,101 @@ if (backBTN) {
 // rubric builder page
 
 var saveBTN= document.getElementById("rubric-save");
-console.log(saveBTN);
-var form = document.getElementById("#rubricform");
 
-saveBTN.addEventListener('click',check_skill_checked,true);
+// saveBTN.addEventListener('click',check_skill_checked,true);
 // check if curriculum code is selected
-saveBTN.addEventListener('click',check_cirriculum_code_selected, true);
+saveBTN.addEventListener('click',check_input_filled, true);
 
 
-saveBTN.addEventListener('click',function(e){
-    e.preventDefault();
-    check_cirriculum_code_selected();
-    check_skill_checked();
-    if(check_cirriculum_code_selected()&&check_required_inputs()&&check_skill_checked()){
-        e.currentTarget.submit();
-    }
-
-});
+// saveBTN.addEventListener('click',function(e){
+//
+//     // check_cirriculum_code_selected();
+//     // check_skill_checked();
+//     // if(check_cirriculum_code_selected()&&check_required_inputs()&&check_skill_checked()){
+//     //
+//     // }e.currentTarget.submit();
+//
+// });
 
 // check curriculum code is selected
-function check_cirriculum_code_selected(){
-    var error = "";
-    var curriculum_code = document.getElementById("select_curriculum_code").value;
-    if (curriculum_code == " "){
-        error += "You need a curriculum_code \n";
+function check_input_filled(e){
+
+    e.preventDefault();
+
+    var error = "",
+        curriculum_code = document.getElementById("select_curriculum_code").value,
+        skill_1_error = false,
+        skill_2_error = false,
+        title_1_error = false,
+        title_2_error = false;
+    if (curriculum_code === ""){
+        error += "you need curriculum code \n";
     }
 
-    var skill_items = document.getElementById("check-array2").querySelector(".skill-checkbox2");
-    console.log(skill_items);
+    // check rubric title 1 & 2 are entered
+    var rubric_title_1 = document.getElementById("assessment_name1").value;
+    var rubric_title_2 = document.getElementById("assessment_name2").value;
+    var title_1_filled = false;
+    var title_2_filled = false;
+    if (rubric_title_1===""){
+        title_1_error = true;
+        error+="you need to set term 1 rubric title. \n";
+    }
 
+    if (rubric_title_2===""){
+        title_2_error = true;
+        error+="you need to set term 2 rubric title. \n";
+    }
+
+
+    //check form 1 skill items
+    var skill_items_1 = document.getElementById("check-array1").querySelectorAll(".skill-checkbox1");
+    var skill_checked_1 = false;
+    for (var i = 0; i < skill_items_1.length; i++) {
+        if (skill_items_1[i].checked) {
+            skill_checked_1 = true;
+            break;
+        }
+    }
+
+    if (skill_checked_1 === false){
+        skill_1_error = true;
+    }
+
+    if (skill_1_error === true) {
+        error += "you need to select skills for term 1 \n";
+    }
+
+    //check form 2 skill item
+    var skill_items_2 = document.getElementById("check-array2").querySelectorAll(".skill-checkbox2");
+    var skill_checked_2 = false;
+    for(var i=0; i< skill_items_2.length; i++){
+        if (skill_items_2[i].checked) {
+            skill_checked_2 = true;
+            break;
+        }
+    }
+    if (skill_checked_2 === false){
+        skill_2_error = true;
+    }
+    if (skill_2_error === true){
+        error += "you need to select skills for term 2 \n";
+    }
+
+    if((skill_checked_1 === true)&&(skill_checked_2 === true)&&(error === "")){
+        var form = document.getElementById("rubricform");
+        form.submit();
+    }
+    else {
+        alert(error);
+    }
 }
 
 // check at least one skill- radio is selected
-function check_skill_checked(){
-    var error="";
-
-}
+// function check_skill_checked(){
+//     var error="";
+//
+// }
 
 //init function (only executes when onload)
 function init() {
