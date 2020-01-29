813f508c593459360980e89ad90f6809ec8cc9e3
Michael Tonini <michael.jack.tonini@gmail.com>
2020-01-29 21:35:44 +1100

    updated import script, 403 on failed login.

--- import.sql
+++ import.sql
@@ -13,6 +13,7 @@ insert into schools(name, curriculum_details_curriculum_details_Id, school_type_
 insert into schools(name, curriculum_details_curriculum_details_Id, school_type_identifier_id) values('Mount Elwood Primary', 1, 1);
 insert into schools(name, curriculum_details_curriculum_details_Id, school_type_identifier_id) values('River Primary', 2, 1);
 insert into schools(name, curriculum_details_curriculum_details_Id, school_type_identifier_id) values('Armadale Primary', 1, 1);
+insert into schools(name, curriculum_details_curriculum_details_Id, school_type_identifier_id) values('Cowes Primary', 1, 1);
 insert into scriibi_levels(scriibi_Level) values (	-5.00	);
 insert into scriibi_levels(scriibi_Level) values (	-4.75	);
 insert into scriibi_levels(scriibi_Level) values (	-4.50	);
