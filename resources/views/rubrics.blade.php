3d4bb7765368275bfcd6f0fee048d6bcf9de70fd
Michael Tonini <michael.jack.tonini@gmail.com>
2020-01-30 10:56:58 +1100

    renamingimages

--- resources/views/rubrics.blade.php
+++ resources/views/rubrics.blade.php
@@ -60,7 +60,7 @@
                                         <ul class="list-group list-group-flush ">
                                             <li class="text-white m-0 d-flex justify-content-start px-2">
                                                 <!-- load icon address-->
-                                                <img src="/trait-icon/{{$to->getIcon()}}" alt="ideas" class="align-self-center">
+                                                <img src="/images/trait-icon/{{$to->getIcon()}}" alt="ideas" class="align-self-center">
                                                 <!-- load trait title -->
                                                 <span class="skill-title w-100 pl-0 align-self-center px-2">
                                                     <input type="text" name="trait_id" value={{$to->getId()}} hidden />
