a0c096b3deffafe27cff8abf3a2afa099c11f324
Michael Tonini <michael.jack.tonini@gmail.com>
2020-01-29 15:56:00 +1100

    update script

--- import.sql
+++ import.sql
@@ -11,7 +11,7 @@ insert into school_types(fk_curriculum_id, fk_school_type_identifier_id) values(
 insert into school_types(fk_curriculum_id, fk_school_type_identifier_id) values(3, 2);
 insert into schools(name, curriculum_details_curriculum_details_Id, school_type_identifier_id) values('Hawthorn Primary', 1, 1);
 insert into schools(name, curriculum_details_curriculum_details_Id, school_type_identifier_id) values('Mount Elwood Primary', 1, 1);
-insert into schools(name, curriculum_details_curriculum_details_Id, school_type_identifier_id) values('River Primary', 2, 3);
+insert into schools(name, curriculum_details_curriculum_details_Id, school_type_identifier_id) values('River Primary', 2, 1);
 insert into schools(name, curriculum_details_curriculum_details_Id, school_type_identifier_id) values('Armadale Primary', 1, 1);
 insert into scriibi_levels(scriibi_Level) values (	-5.00	);
 insert into scriibi_levels(scriibi_Level) values (	-4.75	);
