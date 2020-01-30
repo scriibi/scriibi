46c6857452c2ed97a83c24ccce3d5868aeb23aff
Michael Tonini <michael.jack.tonini@gmail.com>
2020-01-30 14:16:43 +1100

    removed flag and turned off migrations

--- resources/views/rubrics.blade.php
+++ resources/views/rubrics.blade.php
@@ -75,9 +75,9 @@
                                                     <label class="frm_checkbox">
                                                         <input type="checkbox" class="skill-checkbox1" name="rubric1_skills[]" value={{$skill->getId()}} / >
                                                         <span class="skill-name">{{$skill->getName()}}</span>
-                                                        @if ($skill->getFlag() === true)
+                                                        <!-- @if ($skill->getFlag() === true)
                                                             <img class="skill-flag-icon float-right" src="/images/flag.png" />
-                                                        @endif
+                                                        @endif -->
                                                     </label>
                                                     <span class="skill-tooltip">{!!$skill->getDefinition()!!}</span>
                                                 </li>
