c02586510e72c441c5ee86ad6a0511cd10f1c640
Michael Tonini <michael.jack.tonini@gmail.com>
2020-02-04 09:57:56 +1100

    added comments field

--- database/migrations/2020_01_10_232640_create_tasks_students_table.php
+++ database/migrations/2020_01_10_232640_create_tasks_students_table.php
@@ -18,6 +18,7 @@ class CreateTasksStudentsTable extends Migration
             $table->bigInteger('result')->unsigned();
             $table->bigInteger('student_Id')->unsigned();
             $table->bigInteger('tasks_skills_Id')->unsigned();
+            $table->string('comment');
 
             $table->unique('tasks_students_Id');
 
