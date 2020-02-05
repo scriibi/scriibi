83db0e3ac96670cac6fc862d047154bc9a9bc1a4
Chamath Vindula <chamathvin@gmail.com>
2020-02-05 13:12:01 +1100

    bug-fix student assessment range

---
+++ database/migrations/2020_02_05_020909_student_table_school_field_set_to_nullable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class StudentTableSchoolFieldSetToNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('nullable', function (Blueprint $table) {
+            Schema::table('students', function (Blueprint $table) {
+                $table->string('name', 50)->nullable()->change();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('nullable', function (Blueprint $table) {
+            //
+        });
+    }
+}
