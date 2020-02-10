6b4caabccc64fcb614543d4dbb1d95eda995777f
Angelo Innocente <angelot.innocente@gmail.com>
2020-02-10 11:52:36 +1100

    javascript displays exmples depending on level

--- resources/views/assessment-marking.blade.php
+++ resources/views/assessment-marking.blade.php
@@ -159,7 +159,8 @@
 
             <div class="mt-2 mb-5">
                 <p class="student-table-label w-100 mb-0">Assessed Level:</p>
-                <p id="marking-level">{{$assessed_level}}</p>
+                <p>{{$assessed_level[0]->assessed_level_label}}</p>
+                <p id="marking-level" class="d-none">{{$assessed_level_scriibi_id}}</p>
             </div>
             <div class="d-flex">
                 <p class="student-table-label mb-0">Student Leveled Samples</p>
@@ -197,6 +198,7 @@
                 <div id="level-6" class="d-none">
                     <a target="_blank" class="d-block mb-1" href="https://writing.scriibi.com/wp-content/uploads/scaffoldsetc/Year%20Level%20Assessment%20Samples/Sample%20Level%205.pdf">Level 5</a>
                     <a target="_blank" class="d-block mb-1" href="https://writing.scriibi.com/wp-content/uploads/scaffoldsetc/Year%20Level%20Assessment%20Samples/Sample%20Level%206.pdf">Level 6</a>
+                    <a target="_blank" class="d-block mb-1" href="https://writing.scriibi.com/wp-content/uploads/scaffoldsetc/Year%20Level%20Assessment%20Samples/Sample%20Level%207.pdf">Level 7</a>
                 </div>
             </div>
             <div class="mt-5" >
