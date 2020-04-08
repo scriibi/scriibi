9bc2a97b5ed796075b1892371dff8e8fad079c71
Chamath Vindula <chamathvin@gmail.com>
2020-04-08 14:47:50 +1000

    goal sheet css put into new directory in public

--- resources/views/goalSheet.blade.php
+++ resources/views/goalSheet.blade.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
- <title>Goal Sheets</title>
- <link rel="stylesheet" href="/css/goalsheet-page.css">
-
- 
+	<title>Goal Sheets</title>
+	<link rel="stylesheet" href="./goalSheetCss/goalsheet-page.css">
 </head>
 <body>
 	<div class="container page-break">
